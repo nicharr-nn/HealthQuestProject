930cee8f5932b4360fa2274c1a2ab2e2daf9d00f
Punnapa Pramotekul <punnapa.pr@ku.th>
2025-09-27 22:09:09 +0700

    feature(api): add api specific user update & delete

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("select-goal/", views.set_goal, name="select-goal"),
     path("update-profile/", views.update_profile, name="update-profile"),
     path("upload-photo/", views.upload_photo, name="upload-photo"),
+    path("users/<int:id>/", views.get_user_by_id, name="get-user-by-id"),
+    path("users/<int:id>/update/", views.update_user_by_id, name="update-user-by-id"),
+    path("users/<int:id>/delete/", views.delete_user_by_id, name="delete-user-by-id"),
+    path("workout-programs/", views.workout_programs, name="workout-programs"),
+    path("workout-assignments/<int:id>/", views.workout_assignment_detail, name="workout-assignment-detail"),
 ]
