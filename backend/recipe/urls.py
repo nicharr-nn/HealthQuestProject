ac4e9ffb78205ac523a0fcbf6d8069765b835eaa
Punnapa Pramotekul <punnapa.pr@ku.th>
2025-10-10 23:59:51 +0700

    feature(api): adjust recipe path

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recipes/', views.recipe_list, name='recipe-list'),
-    path('recipes/<int:pk>/', views.recipe_detail, name='recipe-detail'),
-    path('recipes/<int:id>/download-pdf/', views.download_recipe_pdf, name='download-recipe-pdf'),
+    path('', views.recipe_list, name='recipe-list'),
+    path('<int:id>/', views.recipe_detail, name='recipe-detail'),
+    path('<int:id>/download-pdf/', views.download_recipe_pdf, name='download-recipe-pdf'),
 ]
