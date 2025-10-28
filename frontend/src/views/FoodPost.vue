ad099a55bc04a514e10274cb3a07f1ac31b0117c
nicharr-nn <nicha.rua@gmail.com>
2025-10-28 15:27:28 +0700

    fix(food-post): adjust image display and text & button colors

--- frontend/src/views/FoodPost.vue
+++ frontend/src/views/FoodPost.vue
@@ -21,11 +21,11 @@
       class="max-w-5xl mx-auto bg-purple-200 shadow-lg rounded-2xl my-8 grid lg:grid-cols-[1.5fr_1fr] overflow-hidden"
     >
       <!-- Left side (image, name, content) -->
-      <div class="flex flex-col font-body">
+      <div class="bg-[#e2dbff] flex flex-col font-body">
         <!-- Image Section -->
         <div class="flex flex-col items-center text-center p-6 text-[#846757]">
           <h3 class="text-3xl font-semibold mb-4">{{ post.title }}</h3>
-          <div class="w-48 h-48 rounded-lg overflow-hidden shadow-md mx-auto cursor-pointer">
+          <div class="w-full h-64 md:h-80 rounded-lg overflow-hidden shadow-md mx-auto cursor-pointer">
             <img
               :src="getImageUrl(post.image)"
               :alt="post.title"
@@ -73,7 +73,7 @@
             </button>
             <button
               @click="deletePost(post.id)"
-              class="cursor-pointer bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-lg font-semibold shadow-md"
+              class="cursor-pointer bg-red-400 hover:bg-red-500 text-white px-5 py-2 rounded-lg font-semibold shadow-md"
             >
               Delete
             </button>
@@ -84,7 +84,7 @@
       <!-- Right side (Comments) -->
       <div class="bg-[#E6F3E6] p-6 flex flex-col justify-between font-body">
         <h4 class="text-xl font-semibold mb-3">Coach Comments</h4>
-        <div class="flex-1 overflow-y-auto space-y-2 mb-4 max-h-60">
+        <div class="flex-1 overflow-y-auto space-y-2 mb-4 max-h-90">
           <div
             v-for="comment in comments[post.id] || []"
             :key="comment.id"
